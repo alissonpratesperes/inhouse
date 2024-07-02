f78ce58eb35a8209359b0e9641ab138164583a1a
Alisson Prates Peres <a.pratesperes@gmail.com>
2024-07-02 20:10:06 -0300

    Minor adjusts after all implementations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { AppService } from "./app.service";
-import { AppController } from "./app.controller";
 import { TagModule } from "./domain/tag/tag.module";
 import { ClientModule } from "./domain/client/client.module";
 import { dataSourceOptions } from "./core/databases/connection";
 
 @Module({
-  providers: [AppService],
-  controllers: [AppController],
+  providers: [],
+  controllers: [],
   imports: [TypeOrmModule.forRoot(dataSourceOptions), ClientModule, TagModule]
 })
 export class AppModule { };
